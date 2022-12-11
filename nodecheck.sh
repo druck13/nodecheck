7fd3ecfdc1182169079b85a2c591fb1095cc9948
David Ruck <david.ruck@pexip.com>
2022-12-11 16:56:20 +0000

    Pass additional arguments to program

--- nodecheck.sh
+++ nodecheck.sh
@@ -5,4 +5,4 @@ mount -t nfs 192.168.0.1:/mnt/share1/test /mnt/n1/share1/test
 mkdir -p /mnt/n2/share1/test
 mount -t nfs 192.168.0.2:/mnt/share1/test /mnt/n1/share1/test
 
-nodecheck.py /mnt/n1/share1/test /mnt/n1/share1/test
+nodecheck.py /mnt/n1/share1/test /mnt/n1/share1/test $@
